06200cc6c3fb6b6aa0ea86c2a7e1a6487b2c0c63
monochromegane <dev.kuro.obi@gmail.com>
2013-11-24 22:09:36 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CommitActivityGraph::Application.config.secret_key_base = '2f1a27752180374a67325d185c8f9df437d68b8c083a4955e98fef3ea52babfbebb545327a6f79848f575897c774bfda5cdc7d034b3aff35bad027b2d5bd0707'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CommitActivityGraph::Application.config.session_store :cookie_store, key: '_commit_activity_graph_session'
